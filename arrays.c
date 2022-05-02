a785ab802854a5fb6a681fe23d752964bd2e4c57
m-chan <mantingchan2@gmail.com>
2022-05-02 19:22:18 +0100

    creating the array and editting it

--- arrays.c
+++ arrays.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 
 int main(){
+    int arrayOfNumbers[] = {1, 2, 3, 5, 6}; // creates an array of integers with indexes 0+
+
+    printf("The first entry of the array is: %d \n", arrayOfNumbers[0]);
+
+    arrayOfNumbers[0] = 20;
+
+    printf("Now it is: %d", arrayOfNumbers[0]);
+    
     return 0;
 }
