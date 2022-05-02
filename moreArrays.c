8bdbb0484950b49d1ee3f578fb343c9413328118
m-chan <mantingchan2@gmail.com>
2022-05-02 19:34:17 +0100

    creating a 2d array

--- moreArrays.c
+++ moreArrays.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 
 int main(){
-
+    // creating a 2d array (grid) of integers...
+    int gridArray2d[4][3] = {{1, 2, 3},
+                             {5, 7, 9}, 
+                             {13, 14, 17},
+                             {21, 39, 45}}; // 4 'main' entries in array due to [4][] & 3 in each entry due to [][3]
     
     return 0;
 }
