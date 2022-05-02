73d4a59af3d41eec450942c3b7c5c937375863a3
m-chan <mantingchan2@gmail.com>
2022-05-02 19:37:53 +0100

    printing out entries of the 2d array

--- moreArrays.c
+++ moreArrays.c
@@ -6,6 +6,9 @@ int main(){
                              {5, 7, 9}, 
                              {13, 14, 17},
                              {21, 39, 45}}; // 4 'main' entries in array due to [4][] & 3 in each entry due to [][3]
+
+    printf("%d\n", gridArray2d[2][0]); // should be 13
+    printf("%d\n", gridArray2d[3][2]); // should be 45
     
     return 0;
 }
