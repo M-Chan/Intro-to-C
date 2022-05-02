5eaebaa68a20fc21de548c1b61caa6f0f3bdfff4
m-chan <mantingchan2@gmail.com>
2022-05-02 19:54:16 +0100

    created a multiplying function

--- basicFuncs.c
+++ basicFuncs.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 
+int number1, number2;
+
+// creating a function called multiplier with two integer inputs
+int multiplier(int a, int b){
+    return a*b;
+}
+
 int main(){
-   
-    
+    printf("Please enter a number: ");
+    scanf("%d", &number1);
+    printf("Please enter another number: ");
+    scanf("%d", &number2);
+
+    printf("The product of %d and %d is %d", number1, number2, multiplier(number1, number2)); // inputs the numbers to function
     return 0;
 }
