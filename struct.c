d074e8b6068bba328ff8706eec8d146d2fa1ff52
m-chan <mantingchan2@gmail.com>
2022-05-03 11:48:40 +0100

    chess piece struct

--- struct.c
+++ struct.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 
+// Creating the basic struct (like a 'template' / 'skeleton')
+struct chessPiece {
+    char *colour;
+};
+
 int main() {
 
+    // Using the skeleton to build a white queen chess piece
+    struct chessPiece whiteQueen;
+    whiteQueen.colour = "white";
+    
+    printf("The chess piece is %s\n", whiteQueen.colour);
     return 0;
 }
